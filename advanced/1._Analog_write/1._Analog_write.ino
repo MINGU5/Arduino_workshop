2bee483b16e7c78d6c92beaa7e8448336a7c1737
MINGU5 <56580258+MINGU5@users.noreply.github.com>
2019-10-15 20:05:02 +0900

    ADVANCED PRACTICE

---
+++ advanced/1._Analog_write/1._Analog_write.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(3, OUTPUT);
+}
+
+void loop() {
+   analogWrite(3,133);
+   delay(500);
+   analogWrite(3,255);
+   delay(500);
+
+  
+  // put your main code here, to run repeatedly:
+
+}
